1d172eaf03ea538af50fa07b9a8a0a18247d73a5
Taehwan An <conscious52th@gmail.com>
2025-07-20 10:22:06 -0600

    1. Initialize project

---
+++ src/main/java/com/example/heroku_api_gateway/HerokuApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.heroku_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuApiGatewayApplication.class, args);
+	}
+
+}
