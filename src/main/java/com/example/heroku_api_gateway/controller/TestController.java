061548d3d88a0c488152ab10ccf60748de44a207
Taehwan An <conscious52th@gmail.com>
2025-07-23 15:04:23 -0600

    8. Modify app.json for Heroku CI test and Add TestController

---
+++ src/main/java/com/example/heroku_api_gateway/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.heroku_api_gateway.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+  private final String rootPath = "/test";
+
+  @GetMapping(rootPath)
+  public String main(){
+    return "Hello This is Test Controller.";
+  }
+}
